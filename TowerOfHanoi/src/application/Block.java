867135bee3e184fcb37bbfbe0a058527ab1d7631
senay kula <senaydemirkula@gmail.com>
2024-09-26 00:28:23 -0400

    This game involves moving a tower of discs on one peg to another without placing the larger discs of the tower on top of the smaller discs. The user can configure the game when logged in. (Number of nails, blocks, etc.)

---
+++ TowerOfHanoi/src/application/Block.java
@@ -0,0 +1,16 @@
+package application;
+
+
+import javafx.scene.shape.Rectangle;
+
+public class Block {
+
+    public Rectangle block;
+    public int width;
+
+    Block(Rectangle block, int width) {
+
+        this.width = width;
+        this.block = block;
+    }
+}
